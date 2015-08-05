e9ee58cd58ec8065961e98e9714be517cfde6bd3
Server <calumgathergood@gmail.com>
2015-08-05 20:41:31 +0100

    Added tabs for each section. Working on implementing the FAB info button

--- Material Test/app.js
+++ Material Test/app.js
@@ -1,8 +1,18 @@
-(function(){
+(function () {
     'use strict';
 
-    angular.module('myApp', ['ngMaterial'])
-        .controller('ListController', ['$scope', function($scope){
+    var app = angular.module('myApp', ['ngMaterial', 'ng-mfb']);
 
-        }]);
-})
+    app.controller('AppCtrl', AppCtrl);
+    function AppCtrl ( $scope ) {
+        $scope.data = {
+            selectedIndex: 0
+        };
+        $scope.next = function() {
+            $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2) ;
+        };
+        $scope.previous = function() {
+            $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
+        };
+    }
+})();
