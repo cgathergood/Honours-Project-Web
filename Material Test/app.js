6b43064455b469d9cb35dceb32218cb96a872c68
Server <calumgathergood@gmail.com>
2015-08-05 22:51:22 +0100

    Cleaned up existing code. Controller and css are bare bones at the moment

--- Material Test/app.js
+++ Material Test/app.js
@@ -1,18 +1,1 @@
-(function () {
-    'use strict';
-
-    var app = angular.module('myApp', ['ngMaterial']);
-
-    app.controller('AppCtrl', AppCtrl);
-    function AppCtrl ( $scope ) {
-        $scope.data = {
-            selectedIndex: 0
-        };
-        $scope.next = function() {
-            $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2) ;
-        };
-        $scope.previous = function() {
-            $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
-        };
-    }
-})();
+var app = angular.module('HonoursApp', ['ngMaterial']);
