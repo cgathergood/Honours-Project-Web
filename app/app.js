618661360eac322ed55a7dfa2d16342949a001ec
Server <calumgathergood@gmail.com>
2015-08-05 22:57:21 +0100

    No longer using material test folder. This is a decent first attempt at the web component. Will start playing parse shortly

--- app/app.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Calum on 04/08/2015.
- */
-var app = angular.module("myNoteApp", []);
--- app/controllers/myNoteCtrl.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Calum on 04/08/2015.
- */
-app.controller("myNoteCtrl", function($scope) {
-    $scope.message = "";
-    $scope.left  = function() {return 100 - $scope.message.length;};
-    $scope.clear = function() {$scope.message = "";};
-    $scope.save  = function() {alert("Note Saved");};
-});
